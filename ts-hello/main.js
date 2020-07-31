d75828886d0c1783c1ead7deebfc77d281f954d6
VU Tuan Trung <trung.lespace@gmail.com>
2020-07-31 22:23:28 +0200

    Adding some tutorial samples
    _Types (const, types)
    _Annotation type
    _Arrow function
    _Interface

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,3 +1,66 @@
+// Types
+{
+    let testing = 1;
+    // Example of unsetting variable type
+    let unknownType;
+    unknownType = 1;
+    unknownType = true;
+    unknownType = 'a';
+    // Example of setting variable type
+    let numberType;
+    let booleanType;
+    let stringType;
+    let anyType;
+    anyType = 1;
+    anyType = true;
+    let numberArr = [1, 2, 5];
+    let anyArr = [true, 1, 'a'];
+    // Constant
+    const COLORRED = 0;
+    const COLORGREEN = 1;
+    const COLORBLUE = 2;
+    // Enum
+    let Color;
+    (function (Color) {
+        Color[Color["Red"] = 0] = "Red";
+        Color[Color["Blue"] = 1] = "Blue";
+        Color[Color["Green"] = 2] = "Green";
+    })(Color || (Color = {}));
+    ;
+    let bgColor = Color.Red;
+}
+// Assertion type
+{
+    let message;
+    message = 'abc';
+    let endsWithC = message.endsWith('c');
+    let alternativeWay = message.endsWith('c');
+}
+// Arrow function
+{
+    let log = function (message) {
+        console.log(message);
+    };
+    let doLog1 = (message) => { console.log(message); };
+    let doLog2 = message => console.log(message);
+    let returningOne = () => { return 1; };
+}
+// Interfaces
+{
+    let drawPoint1 = (x, y) => { };
+    let drawPoint2 = (point) => { }; // Inline annotation
+    let drawPoint3 = (point) => { };
+}
+// Cohension
+{
+}
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log("Finally: " + i);
+}
+doSomething();
 function log(message) {
     console.log(message);
 }
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,4 +1,83 @@
-function log(message){
+// Types
+{
+    let testing = 1;
+
+    // Example of unsetting variable type
+    let unknownType;
+    unknownType = 1;
+    unknownType = true;
+    unknownType = 'a';
+    // Example of setting variable type
+    let numberType: number;
+    let booleanType: boolean;
+    let stringType: string;
+    let anyType: any;
+    anyType = 1;
+    anyType = true;
+
+    let numberArr: number[] = [1, 2, 5];
+    let anyArr: any[] = [true, 1, 'a'];
+
+    // Constant
+    const COLORRED = 0;
+    const COLORGREEN = 1;
+    const COLORBLUE = 2;
+
+    // Enum
+    enum Color { Red = 0, Blue = 1, Green = 2 };
+    let bgColor = Color.Red;
+}
+
+
+// Assertion type
+{
+    let message;
+    message = 'abc';
+    let endsWithC = (<String>message).endsWith('c');
+    let alternativeWay = (message as String).endsWith('c');
+}
+
+// Arrow function
+{
+    let log = function (message) {
+        console.log(message);
+    }
+
+    let doLog1 = (message) => { console.log(message); }
+    let doLog2 = message => console.log(message);
+    let returningOne = () => { return 1; }
+}
+
+// Interfaces
+{
+    let drawPoint1 = (x, y) => { };
+    let drawPoint2 = (point: { x: number, y: number }) => { };// Inline annotation
+
+    // We do not want to create this kind of declaration all time, so we create Interface
+    interface Point{
+        x: number,
+        y: number
+    }
+
+    let drawPoint3 = (point: Point) => { }
+}
+
+// Cohension
+{
+
+}
+
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+
+    console.log("Finally: " + i);
+}
+
+doSomething();
+
+function log(message) {
     console.log(message);
 }
 
