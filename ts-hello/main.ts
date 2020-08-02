71db97bf3c5bfb48053c4e2849d156056db036d8
VU Tuan Trung <trung.lespace@gmail.com>
2020-08-02 19:57:11 +0200

    Adding some tutorial samples
    _Class object
    _Constructor
    _Access modifier
    _Properties

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -54,18 +54,130 @@
     let drawPoint2 = (point: { x: number, y: number }) => { };// Inline annotation
 
     // We do not want to create this kind of declaration all time, so we create Interface
-    interface Point{
+    interface Point {
         x: number,
-        y: number
+        y: number,
+        draw: () => void
     }
 
     let drawPoint3 = (point: Point) => { }
 }
 
-// Cohension
+// Class object
 {
+    // create a class object
+    class Point {
+        x: number;
+        y: number;
 
+        draw() {
+            console.log('X: ' + this.x + ', Y : ' + this.y);
+        }
+
+        getDistance(anotherPoint: Point) {
+            // ...
+        }
+    }
+
+    let point = new Point();
+    point.x = 1;
+    point.y = 2;
+    point.draw();
 }
+
+// Constructor
+{
+    class Point {
+        x: number;
+        y: number;
+
+        // In typescript we can not have multiple constructors
+        // x: number => x?: number make x a optional parameter
+        // Rule: if set normal parameter to optional parameter, we have to declare all the parameters on it's right parameters optional
+        constructor(x?: number, y?: number) {
+            this.x = x;
+            this.y = y;
+        }
+
+        draw() {
+            console.log('X: ' + this.x + ', Y : ' + this.y);
+        }
+    }
+
+    let point = new Point();
+    point.draw();
+}
+
+
+// Access modifier: public, private, protected
+{
+    class ConcreatePointA {
+        private x: number;
+        private y: number;
+
+        // In typescript we can not have multiple constructors
+        // x: number => x?: number make x a optional parameter
+        // Rule: if set normal parameter to optional parameter, we have to declare all the parameters on it's right parameters optional
+        constructor(x?: number, y?: number) {
+            this.x = x;
+            this.y = y;
+        }
+
+        draw() {
+            console.log('X: ' + this.x + ', Y : ' + this.y);
+        }
+    }
+
+    let point = new ConcreatePointA();
+    point.draw();
+
+
+    class ConcreatePointB {
+        // In typescript we can not have multiple constructors
+        // x: number => x?: number make x a optional parameter
+        // Rule: if set normal parameter to optional parameter, we have to declare all the parameters on it's right parameters optional
+        constructor(private x?: number, private y?: number) {
+        }
+
+        draw() {
+            console.log('X: ' + this.x + ', Y : ' + this.y);
+        }
+    }
+}
+
+
+// Properties
+{
+    class ConcreatePoint {
+        // In typescript we can not have multiple constructors
+        // x: number => x?: number make x a optional parameter
+        // Rule: if set normal parameter to optional parameter, we have to declare all the parameters on it's right parameters optional
+        constructor(private _x?: number, private _y?: number) {
+        }
+
+        draw() {
+            console.log('X: ' + this._x + ', Y : ' + this._y);
+        }
+
+        // Setter method
+        get x() {
+            return this._x;
+        }
+
+        // Getter method
+        set x(value) {
+            if (value < 0)
+                throw new Error('value cannot be less than 0.');
+
+            this._x = value;
+        }
+    }
+
+    let point = new ConcreatePoint(1, 2);
+    let x = point.x;
+    point.x = 10;
+}
+
 
 function doSomething() {
     for (var i = 0; i < 5; i++) {
