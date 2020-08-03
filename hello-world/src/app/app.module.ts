a9ece978c1f396f2aec78962af2e66a88e9b7c5a
VU Tuan Trung <trung.lespace@gmail.com>
2020-08-03 23:43:43 +0200

    Create new component by using CLI

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
+  // declaration: this is where we add all the components that are part if this module
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
