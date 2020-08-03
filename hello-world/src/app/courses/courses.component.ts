6b3fca841db32dc4903a7be8ce0e77f0023b3188
VU Tuan Trung <trung.lespace@gmail.com>
2020-08-03 23:51:49 +0200

    Create custom template + Add new component to app component html

--- hello-world/src/app/courses/courses.component.ts
+++ hello-world/src/app/courses/courses.component.ts
@@ -2,14 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  templateUrl: './courses.component.html',
+  template: '<h2>{{ getTitle() }}</h2>',
+  // templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
 
+  title = 'List of courses';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getTitle(){
+    return this.title;
   }
 
 }
